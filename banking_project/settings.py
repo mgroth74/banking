e64f3e8413a57f99f4bf37deae585a74267a79b0
Grothusen <grothusenmichaelw@johndeere.com>
2020-02-24 16:16:23 -0600

    updated settings

--- banking_project/settings.py
+++ banking_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'mymazumaapp.herokuapp.com']
+ALLOWED_HOSTS = ['mymazumaapp.herokuapp.com']
 
 
 # Application definition
